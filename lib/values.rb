9dd8d9f46d226c6e6d0333a69c53ea1c450764c3
Peter Rose <peterroseq1@gmail.com>
2020-03-15 19:27:32 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -15,7 +15,7 @@ def get_the_values(groceries)
   meat: ["chicken", "steak", "salmon"],
   grains: ["rice", "pasta"]
 }
-  foods = []
+  foods = ""
   foods << groceries.values.flatten
   binding.pry
 end
